303d0458719e6486969aa687e1a14dcf38338b0d
ADILELBOURKI <adilbcsm@gmail.com>
2020-08-13 17:30:00 +0100

    while loop added

--- src/DnaTest.java
+++ src/DnaTest.java
@@ -1,26 +1,26 @@
 
 public class DnaTest {
 	public String findGene(String dna) {
-		
+
 		//1-finding indexes of ATG (start) and TAA (stop)
-		
 		int atgIndex = dna.indexOf("ATG");
-		int taaIndex = dna.indexOf("TAA");
-		if(atgIndex !=-1 & (taaIndex-atgIndex)%3==0) {
-		//2-extract the gene from the DNA 
-		String result ="";
-		result = dna.substring(atgIndex,taaIndex+3);
-		//3-returning the result
-		
-		return result ; 
+		int taaIndex = dna.indexOf("TAA",atgIndex+3);
+		while(taaIndex!=-1) {
+			if((taaIndex-atgIndex)%3==0) {
+				return dna.substring(atgIndex,taaIndex+3);
+			}
+			
+			else {
+				taaIndex=dna.indexOf("TAA",taaIndex+1);
+				
+			}
 		}
-		else 
-			return "----No gene---";
-		}
-	
+		return "";
+}
+
 	//Some test cases
 	public void testOne() {
-		String dna = "ATGTAA";
+		String dna = "AATGCGTAATTAATCG";
 		System.out.println(findGene(dna));
 	}
 	public void testTwo() {
@@ -28,25 +28,25 @@ public class DnaTest {
 		System.out.println(findGene(dna));
 	}
 	public void testThree() {
-		String dna = "GCTTGCTGAATGGCTGTGTGCGATGCTATGTCGTCGTAGCTAAGCTGAT";
+		String dna = "GCTTGCTGAATGTAAGCTGTGTGCGATGCTATGTCGTCGTAGTAAGCTGAT";
 		System.out.println(findGene(dna));
 	}
 	public void testFour() {
 		String dna = "GCTTGCTGAGCTGTGTGCGCTTCGTCGTAGCTAAGCTGAT";
 		System.out.println(findGene(dna));
 	}
-	
-	
+
+
 	public static void main(String[] args) {
-//		String s = "ATGATATGGTCGATGHGTAA";
-		String DNA = "ATGATCGCTAATGCTAAGCTATG";
+//		//		String s = "ATGATATGGTCGATGHGTAA";
+//		String DNA = "ATGATCGCTAATGCTAAGCTATG";
 		DnaTest dnaTest = new DnaTest();
-		System.out.println(dnaTest.findGene(DNA));
-		System.out.println("----------------");
+//		System.out.println(dnaTest.findGene(DNA));
+//		System.out.println("----------------");
 		dnaTest.testOne();
 		dnaTest.testTwo();
 		dnaTest.testThree();
 		dnaTest.testFour();
-		
+
 	}
 }
